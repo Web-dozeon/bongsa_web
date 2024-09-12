6b43d324d4066baaf5d41e98128984d222cb96d5
유희정 <rheejung63@naver.com>
2024-09-12 20:35:19 +0900

    TEST
    test

---
+++ 07_quiz.py
@@ -0,0 +1,14 @@
+# name = input("이름: ")
+# domain = input("도메인: ")
+# print(name + "@" + domain + ".com")
+
+one = input("문장 1:")
+two = input("문장 2:")
+three = input("문장 3:") + "."
+# result = [one, two, three] join을 사용하기 위해서 list로 만들어줘야 함
+print(". ".join([one, two, three]))
+
+# str = input("반복할 문자열을 입력하세요:")
+# count = int(input("반복 횟수를 입력하세요:"))
+# result = str * count
+# print(result)
